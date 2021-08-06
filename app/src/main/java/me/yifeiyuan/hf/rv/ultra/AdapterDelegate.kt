654cab68a9a4ba470c294d60756c8a9fe104558e
mingjue <zhening.czn@alibaba-inc.com>
2021-08-06 16:17:50 +0800

    cleanup code

--- app/src/main/java/me/yifeiyuan/hf/rv/ultra/AdapterDelegate.kt
+++ app/src/main/java/me/yifeiyuan/hf/rv/ultra/AdapterDelegate.kt
@@ -24,47 +24,7 @@ interface AdapterDelegate<T, VH : Component<T>> {
 
     fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH
 
-//    fun onBindViewHolder(
-//        holder: VH,
-//        position: Int,
-//        payloads: MutableList<Any>,
-//        model: T?,
-//        adapter: ComponentAdapter
-//    ) {
-//        holder.bind(model, position, payloads, adapter)
-//    }
-
-//    fun onViewRecycled(component: VH, adapter: ComponentAdapter){
-//        component.onViewRecycled(adapter)
-//    }
-
     fun getItemId(model: Any, position: Int): Long = RecyclerView.NO_ID
 
     fun getItemViewType(model: Any, position: Int):Int
-//
-//    fun onViewAttachedToWindow(component: VH, adapter: ComponentAdapter) {
-//        component.onViewAttachedToWindow(adapter)
-//    }
-//
-//    fun onViewDetachedFromWindow(component: VH, adapter: ComponentAdapter) {
-//        component.onViewDetachedFromWindow(adapter)
-//    }
-//
-//    fun onFailedToRecycleView(component: VH, adapter: ComponentAdapter): Boolean {
-//        return component.onFailedToRecycleView(adapter)
-//    }
-//
-//    fun onDetachedFromRecyclerView(recyclerView: RecyclerView, componentAdapter: ComponentAdapter) {
-//        Log.d(
-//            TAG,
-//            "onDetachedFromRecyclerView() called with: recyclerView = $recyclerView, componentAdapter = $componentAdapter"
-//        )
-//    }
-//
-//    fun onAttachedToRecyclerView(recyclerView: RecyclerView, componentAdapter: ComponentAdapter) {
-//        Log.d(
-//            TAG,
-//            "onAttachedToRecyclerView() called with: recyclerView = $recyclerView, componentAdapter = $componentAdapter"
-//        )
-//    }
 }
